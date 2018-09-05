78e5ff46e8c8b5266e973118ae63aa606a372ece
Sebastian Rotar <sebastianrotar@Sebastians-MBP.lan>
2018-09-05 21:27:50 +0200

    modal done

--- w1d4 - modal/mysolution/script.js
+++ w1d4 - modal/mysolution/script.js
@@ -1,9 +1,30 @@
 "use strict";
 
+let openModal = document.querySelector(".openModal");
+let closeModal = document.querySelector("#closebutton");
+let modal = document.querySelector(".modal");
+
+openModal.addEventListener("click", showModal);
+closeModal.addEventListener("click", hideModal);
+
 function showModal() {
-  console.log("Show modal");
+  modal.style.display = "block";
 }
 
 function hideModal() {
-  console.log("Hide modal");
+  modal.style.display = "none";
 }
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  let modal = document.querySelector(".modal");
+  if (evt.keyCode == 27) {
+    modal.style.display = "none";
+  }
+};
